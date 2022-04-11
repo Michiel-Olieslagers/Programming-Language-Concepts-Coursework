4952ba6989f58478c564385930621be9241ae842
Michiel Olieslagers <44864547+Michiel-Olieslagers@users.noreply.github.com>
2022-04-11 12:21:15 +0200

    Added happy file and start of formatting

--- Alex/Main.hs
+++ Alex/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import System.Environment
 import System.IO
 import TurtleLex
+import TurtlePar
 import Control.Exception
 
 main :: IO ()
@@ -13,3 +14,30 @@ getTokens = do path <- getArgs
                contents <- readFile (path!!0)
                let var = alexScanTokens contents
                print var
+               let parsedProg = parseCalc (alexScanTokens contents)
+               print parsedProg
+
+getBase :: Exp -> Maybe String
+getBase [] = Nothing
+getBase ((Tag _ _):es) = getBase es
+getBase ((PreDef _ _):es) = getBase es
+getBase ((BaseDef x):es) = Just(x)
+
+getBase :: Exp -> [(String,String)]
+getBase [] = []
+getBase ((Tag _ _):es) = getBase es
+getBase ((PreDef x y):es) = ((x,y):getBase es)
+getBase ((BaseDef _):es) = getBase es
+
+format :: Exp -> Maybe String -> [(String,String)] -> [(String,[(String,[String])])]
+format ((Tag (URI x@('h':'t':'t':'p':xs)) y):es) base prefs = ((x,(formatList y base prefs)):format es base prefs)
+format ((Tag (URI x) y):es) base@(Just b) prefs = (((b++x),(formatList y base prefs)):format es base prefs)
+format ((Tag (URI x) y):es) (Nothing) prefs = error "Base definition is not defined so all tripples must use URIs!"
+format ((Tag (URI x) y):es) base prefs = 
+format ((Tag (URI x) y):es) (Nothing) prefs = error "Base definition is not defined so all tripples must use URIs!"
+ 
+
+
+
+
+
