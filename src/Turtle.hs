aa36768257b20f738b6ad739579c99e0cf5e6177
rorywilson <rorywilso99@gmail.com>
2022-04-22 12:54:57 +0100

    Refactored

--- src/Turtle.hs
+++ src/Turtle.hs
@@ -12,25 +12,25 @@ type Triple = (String,String,Obj)
 type MaybTripl = (Maybe String, Maybe String, Maybe Obj)
 
 getTriples :: [String] -> IO [[Triple]]
-getTriples paths = getData paths
+getTriples = getData
 
 getData :: [String] -> IO [[Triple]]
 getData [] = return []
 getData (x:xs) = do var <- readFile x
-                    let var2 = parseCalc (alexScanTokens (var))
+                    let var2 = parseCalc (alexScanTokens var)
                     let var3 = format var2 (getBase var2) (getPrefs var2)
                     rest <- getData xs
-                    return ((out var3):rest)
+                    return (out var3:rest)
 
 output :: [MaybTripl] -> String
 output [] = []
-output ((Just (t1),Just(t2),Just(t3)):ts) = ("<" ++ t1 ++ "><" ++ t2 ++ "><" ++ getObj t3 ++ "> . \n") ++ output ts
-output ((Just (t1),Just(t2),Nothing):ts) = ("<" ++ t1 ++ "><" ++ t2 ++ "> . \n") ++ output ts
-output ((Just (t1),Nothing,Just(t3)):ts) = ("<" ++ t1 ++ "><" ++ getObj t3 ++ "> . \n") ++ output ts
-output ((Nothing,Just(t2),Just(t3)):ts) = ("<" ++ t2 ++ "><" ++ getObj t3 ++ "> . \n") ++ output ts
-output ((Just (t1),Nothing,Nothing):ts) = ("<" ++ t1 ++ "> . \n") ++ output ts
-output ((Nothing,Just(t2),Nothing):ts) = ("<" ++ t2 ++ "> . \n") ++ output ts
-output ((Nothing,Nothing,Just(t3)):ts) = ("<" ++ getObj t3 ++ "> . \n") ++ output ts
+output ((Just t1, Just t2, Just t3):ts) = ("<" ++ t1 ++ "><" ++ t2 ++ "><" ++ getObj t3 ++ "> . \n") ++ output ts
+output ((Just t1, Just t2, Nothing):ts) = ("<" ++ t1 ++ "><" ++ t2 ++ "> . \n") ++ output ts
+output ((Just t1, Nothing, Just t3):ts) = ("<" ++ t1 ++ "><" ++ getObj t3 ++ "> . \n") ++ output ts
+output ((Nothing, Just t2, Just t3):ts) = ("<" ++ t2 ++ "><" ++ getObj t3 ++ "> . \n") ++ output ts
+output ((Just t1, Nothing, Nothing):ts) = ("<" ++ t1 ++ "> . \n") ++ output ts
+output ((Nothing, Just t2, Nothing):ts) = ("<" ++ t2 ++ "> . \n") ++ output ts
+output ((Nothing, Nothing, Just t3):ts) = ("<" ++ getObj t3 ++ "> . \n") ++ output ts
 output (_:ts) = output ts
 
 getObj :: Obj -> String
@@ -38,65 +38,65 @@ getObj (ObjURI x) = x
 getObj (Lit x) = "\"" ++ x ++ "\""
 getObj (Num x) = show x
 getObj (Bl x) = show x
-getObj _ = error "something has gone wrong" 
+getObj _ = error "Something has gone wrong"
 
 out :: Exp -> [Triple]
 out [] = []
-out (e@(Tag (URI x) y):es) | elem e es = out es
-                           | otherwise = ((outList y x)++out es)
+out (e@(Tag (URI x) y):es) | e `elem` es = out es
+                           | otherwise = outList y x ++ out es
 
-outList :: [(Item,[Obj])] -> String -> [Triple]
+outList :: [(Item, [Obj])] -> String -> [Triple]
 outList [] subj = []
-outList (((URI x),y):[]) subj = (outObjs y subj x)
-outList (((URI x),y):es) subj = (outObjs y subj x++outList es subj)
+outList [(URI x, y)] subj = outObjs y subj x
+outList ((URI x, y):es) subj = outObjs y subj x ++ outList es subj
 
 outObjs :: [Obj] -> String -> String -> [Triple]
 outObjs [] subj pred = []
-outObjs (e:[]) subj pred = [(subj,pred,e)]
-outObjs (e:es) subj pred = ((subj,pred,e):outObjs es subj pred)
+outObjs [e] subj pred = [(subj, pred, e)]
+outObjs (e:es) subj pred = (subj, pred, e):outObjs es subj pred
 
 getBase :: Exp -> Maybe String
 getBase [] = Nothing
 getBase ((Tag _ _):es) = getBase es
 getBase ((PreDef _ _):es) = getBase es
-getBase ((BaseDef x):es) = Just(x)
+getBase ((BaseDef x):es) = Just x
 
-getPrefs :: Exp -> [(String,String)]
+getPrefs :: Exp -> [(String, String)]
 getPrefs [] = []
 getPrefs ((Tag _ _):es) = getPrefs es
-getPrefs ((PreDef x y):es) = ((x,y):getPrefs es)
+getPrefs ((PreDef x y):es) = (x, y):getPrefs es
 getPrefs ((BaseDef _):es) = getPrefs es
 
-format :: Exp -> Maybe String -> [(String,String)] -> Exp
+format :: Exp -> Maybe String -> [(String, String)] -> Exp
 format [] base prefs = []
-format ((Tag (URI x@('h':'t':'t':'p':_)) pre):es) base prefs = (Tag (URI x) (formatList pre base prefs):format es base prefs)
-format ((Tag (URI x) pre):es) base@(Just b) prefs = (Tag (URI (b++x)) (formatList pre base prefs):format es base prefs)
-format ((Tag (URI x) pre):es) (Nothing) prefs = error "Base definition is not defined so all tripples must use URIs!"
-format ((Tag (Pre x y) pre):es) base prefs = (Tag (URI((findPrefix x prefs)++y)) (formatList pre base prefs):format es base prefs)
+format ((Tag (URI x@('h':'t':'t':'p':_)) pre):es) base prefs = Tag (URI x) (formatList pre base prefs):format es base prefs
+format ((Tag (URI x) pre):es) base@(Just b) prefs = Tag (URI (b ++ x)) (formatList pre base prefs):format es base prefs
+format ((Tag (URI x) pre):es) Nothing prefs = error "Base definition is not defined so all tripples must use URIs."
+format ((Tag (Pre x y) pre):es) base prefs = Tag (URI(findPrefix x prefs ++ y)) (formatList pre base prefs):format es base prefs
 format (e:es) base prefs = format es base prefs
 
-formatList :: [(Item,[Obj])] -> Maybe String -> [(String,String)] -> [(Item,[Obj])]
+formatList :: [(Item, [Obj])] -> Maybe String -> [(String, String)] -> [(Item, [Obj])]
 formatList [] base prefs = []
-formatList (((URI x@('h':'t':'t':'p':_)),obj):es) base prefs = ((URI x,(formatObjs obj base prefs)):formatList es base prefs)
-formatList (((URI x),obj):es) base@(Just b) prefs = ((URI (b++x),(formatObjs obj base prefs)):formatList es base prefs)
-formatList (((URI x),obj):es) (Nothing) prefs = error "Base definition is not defined so all tripples must use URIs!"
-formatList (((Pre x y),obj):es) base prefs = ((URI ((findPrefix x prefs)++y),(formatObjs obj base prefs)):formatList es base prefs)
+formatList ((URI x@('h':'t':'t':'p':_), obj):es) base prefs = (URI x, formatObjs obj base prefs):formatList es base prefs
+formatList ((URI x, obj):es) base@(Just b) prefs = (URI (b++x), formatObjs obj base prefs):formatList es base prefs
+formatList ((URI x, obj):es) Nothing prefs = error "Base definition is not defined so all tripples must use URIs."
+formatList ((Pre x y, obj):es) base prefs = (URI (findPrefix x prefs ++ y), formatObjs obj base prefs):formatList es base prefs
 
-formatObjs :: [Obj] -> Maybe String -> [(String,String)] -> [Obj]
+formatObjs :: [Obj] -> Maybe String -> [(String, String)] -> [Obj]
 formatObjs [] base prefs = []
-formatObjs ((ObjURI x@('h':'t':'t':'p':_)):es) base prefs = (ObjURI x:formatObjs es base prefs)
-formatObjs ((ObjURI x):es) base@(Just b) prefs = (ObjURI (b++x):formatObjs es base prefs)
-formatObjs ((ObjURI x):es) (Nothing) prefs = error "Base definition is not defined so all tripples must use URIs!"
-formatObjs ((ObjPre x y):es) base prefs = (ObjURI ((findPrefix x prefs)++y):formatObjs es base prefs)
-formatObjs ((Lit x):es) base prefs = (Lit x:formatObjs es base prefs)
-formatObjs ((Bl x):es) base prefs = (Bl x:formatObjs es base prefs)
-formatObjs ((Num x):es) base prefs = (Num x:formatObjs es base prefs)
+formatObjs ((ObjURI x@('h':'t':'t':'p':_)):es) base prefs = ObjURI x:formatObjs es base prefs
+formatObjs ((ObjURI x):es) base@(Just b) prefs = ObjURI (b ++ x):formatObjs es base prefs
+formatObjs ((ObjURI x):es) Nothing prefs = error "Base definition is not defined so all tripples must use URIs."
+formatObjs ((ObjPre x y):es) base prefs = ObjURI (findPrefix x prefs ++ y):formatObjs es base prefs
+formatObjs ((Lit x):es) base prefs = Lit x:formatObjs es base prefs
+formatObjs ((Bl x):es) base prefs = Bl x:formatObjs es base prefs
+formatObjs ((Num x):es) base prefs = Num x:formatObjs es base prefs
 
-findPrefix :: String -> [(String,String)] -> String
-findPrefix x [] = error "Prefix definition not found for used prefix"
+findPrefix :: String -> [(String, String)] -> String
+findPrefix x [] = error "Prefix definition not found for used prefix."
 findPrefix x (y:ys) | fst y == x = snd y
                     | otherwise = findPrefix x ys
- 
+
 
 
 
--- src/stql.hs
+++ src/stql.hs
@@ -21,17 +21,15 @@ main = getTokens
 getTokens :: IO ()
 getTokens = do path <- getArgs
                contents <- readFile (head path)
-               let var = alexScanTokens contents
                let parseVar = parseCalc (alexScanTokens contents)
                str <- parse [] parseVar
-               print (str)
+               print str
 
-parse :: [(String,Vals)] -> Exp -> IO String
-parse _ [] = error "file does not end in an output"
-parse vars (VarAssign var query _:ts) = do newVars <-assign vars var query
-                                           rest <- parse (newVars) ts
-                                           return (rest)
-parse vars (Out var :[]) = do return (out (getVars vars var))
+parse :: [(String, Vals)] -> Exp -> IO String
+parse _ [] = error "File does not end in an output."
+parse vars (VarAssign var query _:ts) = do newVars <- assign vars var query
+                                           parse newVars ts
+parse vars [Out var] = return (out (getVars vars var))
 
 out :: Vals -> String
 out (Trpl triple) = output triple
@@ -39,17 +37,17 @@ out (Numbr int) = show int
 out (Str string) = string
 out (TOF bl) = show bl
 
-getVars :: [(String,Vals)] -> String -> Vals
-getVars [] var = error "variable being printed out does not exist"
+getVars :: [(String, Vals)] -> String -> Vals
+getVars [] var = error "Variable being printed out does not exist."
 getVars (e:es) var | fst e == var = snd e
                    | otherwise = getVars es var
 
-assign :: [(String,Vals)] -> String -> Value -> IO [(String,Vals)]
+assign :: [(String, Vals)] -> String -> Value -> IO [(String, Vals)]
 assign vars var (QueryVal query) = do triples <- getQuery query
                                       return (vars ++ [(var, Trpl triples)])
-assign vars var (StringVal string) = do return (vars ++ [(var, Str string)])
-assign vars var (IntVal int) = do return (vars ++ [(var, Numbr int)])
-assign vars var (BoolVal bl) = do return (vars ++ [(var, TOF bl)])
+assign vars var (StringVal string) = return (vars ++ [(var, Str string)])
+assign vars var (IntVal int) = return (vars ++ [(var, Numbr int)])
+assign vars var (BoolVal bl) = return (vars ++ [(var, TOF bl)])
 
 getQuery :: Query -> IO [MaybTripl]
 getQuery (SelectIF fields file) = do triples <- select file
@@ -63,36 +61,36 @@ getQuery (SelectIFPI fields file _ _) = do triples <- select file
 --union :: 
 
 whr :: [MaybTripl] -> Predicate -> [MaybTripl]
-whr triples (PredICS ('P':'r':'e':'d':[]) item ) = filtr triples True (SubPred (removeLast (tail item)))
-whr triples (PredICS ('O':'b':'j':[]) item) | head item == '<' && lst item == '>' = filtr triples True (Object (ObjURI (removeLast (tail item))))
+whr triples (PredICS ['P', 'r', 'e', 'd'] item ) = filtr triples True (SubPred (removeLast (tail item)))
+whr triples (PredICS ['O', 'b', 'j'] item) | head item == '<' && lst item == '>' = filtr triples True (Object (ObjURI (removeLast (tail item))))
                                             | item == "false" = filtr triples True (Object (Bl False))
                                             | item == "true" = filtr triples True (Object (Bl True))
-                                            | checkNothing (readMaybe item) == True = filtr triples True (Object (Lit item))
+                                            | checkNothing (readMaybe item) = filtr triples True (Object (Lit item))
                                             | otherwise = filtr triples True (Object (Num (read item)))
-whr triples (PredICS ('S':'u':'b':'j':[]) item ) = filtr triples False (SubPred (removeLast (tail item)))
+whr triples (PredICS ['S', 'u', 'b', 'j'] item ) = filtr triples False (SubPred (removeLast (tail item)))
 
 checkNothing :: Maybe Int -> Bool
-checkNothing (Nothing) = True
+checkNothing Nothing = True
 checkNothing _ = False
 
 lst :: String -> Char
 lst [] = ' '
-lst (x:[]) = x
+lst [x] = x
 lst (x:xs) = lst xs
 
 filtr :: [MaybTripl] -> Bool -> Itm -> [MaybTripl]
 filtr [] _ _ = []
-filtr (t@(_,_,Just(t3)):ts) x (Object obj) | t3 == obj = (t:(filtr ts) x (Object obj))
+filtr (t@(_, _, Just t3):ts) x (Object obj) | t3 == obj = t:filtr ts x (Object obj)
                                            | otherwise = filtr ts x (Object obj)
-filtr (t@(_,Just(t2),_):ts) True (SubPred pred) | t2 == pred = (t:(filtr ts True (SubPred pred)))
+filtr (t@(_, Just t2, _):ts) True (SubPred pred) | t2 == pred = t:filtr ts True (SubPred pred)
                                                 | otherwise = filtr ts True (SubPred pred)
-filtr (t@(Just(t1),_,_):ts) False (SubPred sub) | t1 == sub = (t:(filtr ts False (SubPred sub)))
+filtr (t@(Just t1, _, _):ts) False (SubPred sub) | t1 == sub = t:filtr ts False (SubPred sub)
                                                 | otherwise = filtr ts False (SubPred sub)
-filtr _ _ _ = error "item was not selected from file"
+filtr _ _ _ = error "Item was not selected from file."
 
 select :: String -> IO [Triple]
 select s = do tokens <- getTriples [s]
-              return (head (tokens))
+              return (head tokens)
 
 selectItems :: [String] -> Triple -> MaybTripl
 selectItems ["Subj", "Pred", "Obj"] (x, y, z) = (Just x, Just y, Just z)
